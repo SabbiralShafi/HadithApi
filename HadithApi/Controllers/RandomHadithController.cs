af6e1ee83636b536fe8a3ed0c8526b4213a72e99
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-05-05 23:32:13 +0600

    Add project files.

---
+++ HadithApi/Controllers/RandomHadithController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace HadithApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomHadithController : ControllerBase
+    {
+        private readonly Random random = new ();
+        // GET: api/<RandomHadithController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Hadith>> Get()
+        {
+            var hadithJson = System.IO.File.ReadAllText("./hadith.json");
+            var allHadith = JsonConvert.DeserializeObject<IEnumerable<Hadith>>(hadithJson);
+            return Ok(allHadith);
+        }
+
+        // GET api/<RandomHadithController>/single
+        [HttpGet("single")]
+        public ActionResult<Hadith> GetSingle()
+        {
+            List<Hadith> hadiths = new();
+            var hadithJson = System.IO.File.ReadAllText("./hadith.json");
+            hadiths = JsonConvert.DeserializeObject<List<Hadith>>(hadithJson);
+            if (hadiths.Count < 1)
+                return new Hadith { };
+            return Ok(hadiths[random.Next(0, hadiths.Count)]);
+        }
+    }
+}
---
+++ HadithApi/Hadith.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HadithApi
+{
+    public class Hadith
+    {
+        public int ID { get; set; }
+        public string Speaker { get; set; }
+        public string Reference { get; set; }
+        public string Book { get; set; }
+        public string Condition { get; set; }
+        public string Body { get; set; }
+    }
+}
