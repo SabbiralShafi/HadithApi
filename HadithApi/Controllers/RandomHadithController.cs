b816d3bc165392a6833aec3af25fda875df21ef7
Mahmudul Hasan <mahmudnotes@outlook.com>
2021-05-05 23:50:43 +0600

    Fixed json url

--- HadithApi/Controllers/RandomHadithController.cs
+++ HadithApi/Controllers/RandomHadithController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace HadithApi.Controllers
@@ -9,23 +11,29 @@ namespace HadithApi.Controllers
     [ApiController]
     public class RandomHadithController : ControllerBase
     {
-        private readonly Random random = new ();
+        private const string dataPath = "https://raw.githubusercontent.com/Pienteger/HadithApi/master/hadith.json";
+        private readonly Random random = new();
         // GET: api/<RandomHadithController>
         [HttpGet]
-        public ActionResult<IEnumerable<Hadith>> Get()
+        public async Task<ActionResult<IEnumerable<Hadith>>> Get()
         {
-            var hadithJson = System.IO.File.ReadAllText("https://raw.githubusercontent.com/Pienteger/HadithApi/master/hadith.json");
-            var allHadith = JsonConvert.DeserializeObject<IEnumerable<Hadith>>(hadithJson);
+            var http = new HttpClient();
+            var response = await http.GetAsync(dataPath);
+            var result = await response.Content.ReadAsStringAsync();
+            var allHadith = JsonConvert.DeserializeObject<IEnumerable<Hadith>>(result);
             return Ok(allHadith);
         }
 
         // GET api/<RandomHadithController>/single
         [HttpGet("single")]
-        public ActionResult<Hadith> GetSingle()
+        public async Task<ActionResult<Hadith>> GetSingle()
         {
             List<Hadith> hadiths = new();
-            var hadithJson = System.IO.File.ReadAllText("./hadith.json");
-            hadiths = JsonConvert.DeserializeObject<List<Hadith>>(hadithJson);
+            var http = new HttpClient();
+            var response = await http.GetAsync(dataPath);
+            var result = await response.Content.ReadAsStringAsync();
+
+            hadiths = JsonConvert.DeserializeObject<List<Hadith>>(result);
             if (hadiths.Count < 1)
                 return new Hadith { };
             return Ok(hadiths[random.Next(0, hadiths.Count)]);
